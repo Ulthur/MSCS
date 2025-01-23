b60367b9b07a37bb13f3520b11e5198c529a21e3
Ulthur <esuho1@ocdsb.ca>
2025-01-23 12:35:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import kivy
+from kivy.app import App 
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class testGrid(GridLayout):
+    def __init__(self,**kwargs):
+        super(testGrid,self).__init__(**kwargs)
+        self.cols = 4 # how many columns the grid has
+        self.add_widget(Label(text="Name: "))
+        self.name = TextInput(multiline=False) # Multiline false so you cant add new lines
+        self.add_widget(self.name)
+        
+
+
+class MyApp(App):
+    def build(self):
+        return testGrid()
+
+if __name__ == "__main__":
+    MyApp().run()
