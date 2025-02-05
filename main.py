8141ad537d4ec16599fc25ca68ef6ed9ca8acfa8
Ulthur <esuho1@ocdsb.ca>
2025-02-05 12:49:40 -0500

    Added pandas support

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-<<<<<<< Updated upstream
+import pandas as pd
 import datetime
 x = datetime.datetime.now()
-print(str(x.year) + "/" + str(x.month) + "/" + str(x.day))
-input()
-=======
 
+def fetchDay():
+   df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSEmrxJzhnV_wvnd2GkiyuVoBviY8kZOhGhBZd7EsraGpzn-9wmCycgWZXAr8tYXSJiBM2GQ-jeLvIt/pub?gid=0&single=true&output=csv')
+   df = df.sort_values(axis=0,na_position='first')
+   print(df)
+   #for item in df['MONTH']:
+   # print(item)
+   input()
+fetchDay()
 
 import kivy
 from kivy.app import App 
@@ -13,11 +18,13 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 import datetime
 date = datetime.datetime.now()
-formatted_date = str((str(date.year) + "/" + str(date.month) + "/" + str(date.day)))
+
 
 
 class testGrid(GridLayout):
     def __init__(self,**kwargs):
+        formatted_date = str((str(date.year) + "/" + str(date.month) + "/" + str(date.day)))
+       # fetchDay(datetime.date.month,datetime.date.day)
         super(testGrid,self).__init__(**kwargs)
         self.cols = 2 # how many columns the grid has
         self.add_widget(Label(text="The date is "+ formatted_date))
@@ -38,4 +45,3 @@ class MyApp(App):
 
 if __name__ == "__main__":
     MyApp().run()
->>>>>>> Stashed changes
